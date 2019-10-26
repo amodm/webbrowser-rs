a5d047737a682facd762f59c2555f7fcf6722814
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2019-10-25 23:27:33 -0400

    Add kioclient to the Linux/BSD fallback chain after gnome-open
    `kioclient exec` is KDE's equivalent to gvfs-open. In this version using
    a fixed precedence order, it is added after all entries which existed in
    the previous released version to ensure that it will not change the
    behaviour for users of non-KDE desktops.
    
    Were this starting a new major version, I would put kioclient ahead of
    gvfs-open and gnome-open in the precedence order since it's more common to
    find GTK+ applications on KDE desktops (and, thus `gvfs-open` too) than to
    find KDE applications on GNOME desktops.

--- src/lib.rs
+++ src/lib.rs
@@ -272,6 +272,7 @@ fn open_browser_internal(browser: Browser, url: &str) -> Result<ExitStatus> {
             .or_else(|_| -> Result<ExitStatus> { Command::new("xdg-open").arg(url).status() })
             .or_else(|_| -> Result<ExitStatus> { Command::new("gvfs-open").arg(url).status() })
             .or_else(|_| -> Result<ExitStatus> { Command::new("gnome-open").arg(url).status() })
+            .or_else(|_| -> Result<ExitStatus> { Command::new("kioclient").arg("exec").arg(url).status() })
             .or_else(|_| -> Result<ExitStatus> { Command::new("x-www-browser").arg(url).status() }),
         _ => Err(Error::new(
             ErrorKind::NotFound,

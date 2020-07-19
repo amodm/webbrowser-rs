1067faac7b9a5527bbcebaee92ef49ab4eb9e3f9
Amod Malviya <amod@rootnet.in>
2020-07-19 23:38:45 +0530

    Pull windows code out separately

--- src/lib.rs
+++ src/lib.rs
@@ -27,9 +27,9 @@
 //! ```
 
 #[cfg(windows)]
-extern crate widestring;
+mod windows;
 #[cfg(windows)]
-extern crate winapi;
+use windows::open_browser_internal;
 
 use std::default::Default;
 use std::io::{Error, ErrorKind, Result};
@@ -37,19 +37,11 @@ use std::process::{ExitStatus, Output};
 use std::str::FromStr;
 use std::{error, fmt};
 
-#[cfg(windows)]
-use std::os::windows::process::ExitStatusExt;
-#[cfg(windows)]
-use std::ptr;
-
 #[cfg(unix)]
 use std::os::unix::process::ExitStatusExt;
 
 #[cfg(not(windows))]
 use std::process::Command;
-
-#[cfg(windows)]
-use widestring::U16CString;
 
 #[cfg(target_arch = "wasm32")]
 use web_sys::Window;
@@ -203,53 +195,6 @@ pub fn open_browser(browser: Browser, url: &str) -> Result<Output> {
             Err(Error::new(ErrorKind::Other, "interrupted by signal"))
         }
     })
-}
-
-/// Deal with opening of browsers on Windows, using [`ShellExecuteW`](
-/// https://docs.microsoft.com/en-us/windows/desktop/api/shellapi/nf-shellapi-shellexecutew)
-/// fucntion.
-#[cfg(target_os = "windows")]
-#[inline]
-fn open_browser_internal(browser: Browser, url: &str) -> Result<ExitStatus> {
-    use winapi::shared::winerror::SUCCEEDED;
-    use winapi::um::combaseapi::{CoInitializeEx, CoUninitialize};
-    use winapi::um::objbase::{COINIT_APARTMENTTHREADED, COINIT_DISABLE_OLE1DDE};
-    use winapi::um::shellapi::ShellExecuteW;
-    use winapi::um::winuser::SW_SHOWNORMAL;
-    match browser {
-        Browser::Default => {
-            static OPEN: &[u16] = &['o' as u16, 'p' as u16, 'e' as u16, 'n' as u16, 0x0000];
-            let url =
-                U16CString::from_str(url).map_err(|e| Error::new(ErrorKind::InvalidInput, e))?;
-            let code = unsafe {
-                let coinitializeex_result = CoInitializeEx(
-                    ptr::null_mut(),
-                    COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE,
-                );
-                let code = ShellExecuteW(
-                    ptr::null_mut(),
-                    OPEN.as_ptr(),
-                    url.as_ptr(),
-                    ptr::null(),
-                    ptr::null(),
-                    SW_SHOWNORMAL,
-                ) as usize as i32;
-                if SUCCEEDED(coinitializeex_result) {
-                    CoUninitialize();
-                }
-                code
-            };
-            if code > 32 {
-                Ok(ExitStatus::from_raw(0))
-            } else {
-                Err(Error::last_os_error())
-            }
-        }
-        _ => Err(Error::new(
-            ErrorKind::NotFound,
-            "Only the default browser is supported on this platform right now",
-        )),
-    }
 }
 
 /// Deal with opening of browsers on Android
---
+++ src/windows.rs
@@ -0,0 +1,53 @@
+extern crate widestring;
+extern crate winapi;
+
+use crate::{Browser, Error, ErrorKind, ExitStatus, Result};
+use std::os::windows::process::ExitStatusExt;
+use std::ptr;
+use widestring::U16CString;
+
+/// Deal with opening of browsers on Windows, using [`ShellExecuteW`](
+/// https://docs.microsoft.com/en-us/windows/desktop/api/shellapi/nf-shellapi-shellexecutew)
+/// fucntion.
+#[inline]
+pub fn open_browser_internal(browser: Browser, url: &str) -> Result<ExitStatus> {
+    use winapi::shared::winerror::SUCCEEDED;
+    use winapi::um::combaseapi::{CoInitializeEx, CoUninitialize};
+    use winapi::um::objbase::{COINIT_APARTMENTTHREADED, COINIT_DISABLE_OLE1DDE};
+    use winapi::um::shellapi::ShellExecuteW;
+    use winapi::um::winuser::SW_SHOWNORMAL;
+    match browser {
+        Browser::Default => {
+            static OPEN: &[u16] = &['o' as u16, 'p' as u16, 'e' as u16, 'n' as u16, 0x0000];
+            let url =
+                U16CString::from_str(url).map_err(|e| Error::new(ErrorKind::InvalidInput, e))?;
+            let code = unsafe {
+                let coinitializeex_result = CoInitializeEx(
+                    ptr::null_mut(),
+                    COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE,
+                );
+                let code = ShellExecuteW(
+                    ptr::null_mut(),
+                    OPEN.as_ptr(),
+                    url.as_ptr(),
+                    ptr::null(),
+                    ptr::null(),
+                    SW_SHOWNORMAL,
+                ) as usize as i32;
+                if SUCCEEDED(coinitializeex_result) {
+                    CoUninitialize();
+                }
+                code
+            };
+            if code > 32 {
+                Ok(ExitStatus::from_raw(0))
+            } else {
+                Err(Error::last_os_error())
+            }
+        }
+        _ => Err(Error::new(
+            ErrorKind::NotFound,
+            "Only the default browser is supported on this platform right now",
+        )),
+    }
+}

48414a5ebb629ad4db80a088329a396dd42e3ece
Amod Malviya <amod@rootnet.in>
2022-02-17 02:40:36 +0530

    fix linter error for wasm test

--- tests/test_wasm.rs
+++ tests/test_wasm.rs
@@ -17,7 +17,7 @@ mod tests {
     async fn test_wasm32() {
         let uri = &format!("/{}", TEST_PLATFORM);
         let ipv4 = "127.0.0.1";
-        check_request_received_using(uri.into(), &ipv4, |url| {
+        check_request_received_using(uri.into(), ipv4, |url| {
             // modify html to use the correct url
             let mut app_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
             app_dir.push("tests/test-wasm-app");
@@ -26,7 +26,7 @@ mod tests {
             let mut dst_html = PathBuf::from(&app_dir);
             dst_html.push("pkg/test.html");
             let old_html = fs::read_to_string(&src_html).expect("failed to read test.html");
-            let new_html = old_html.replace("DYNAMIC_URL_TBD", &url);
+            let new_html = old_html.replace("DYNAMIC_URL_TBD", url);
             fs::write(&dst_html, &new_html).expect("failed to update dst test.html");
 
             // open browser

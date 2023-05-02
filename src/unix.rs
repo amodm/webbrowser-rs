5a0b5b0c8aab7440a2071d6343169deded35908e
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-05-03 04:19:19 +0530

    Move to the smaller, cargo-team maintained `home` crate

--- src/unix.rs
+++ src/unix.rs
@@ -374,9 +374,15 @@ fn open_using_xdg_config(config_path: &PathBuf, options: &BrowserOptions, url: &
 fn get_xdg_dirs() -> Vec<PathBuf> {
     let mut xdg_dirs: Vec<PathBuf> = Vec::new();
 
-    if let Some(user_dir) = dirs::data_dir() {
-        xdg_dirs.push(user_dir);
+    let data_home = std::env::var("XDG_DATA_HOME")
+        .ok()
+        .map(PathBuf::from)
+        .filter(|path| path.is_absolute())
+        .or_else(|| home::home_dir().map(|path| path.join(".local/share")));
+    if let Some(data_home) = data_home {
+        xdg_dirs.push(data_home);
     }
+
     if let Ok(data_dirs) = std::env::var("XDG_DATA_DIRS") {
         for d in data_dirs.split(':') {
             xdg_dirs.push(PathBuf::from(d));
